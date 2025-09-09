9d0c7fff7a07888c1f374191fb4c407ef32916a1
ShadeStar44 <119338592+ShadeStar44@users.noreply.github.com>
2025-09-09 12:49:05 -0600

    Part 1 File upload

---
+++ FacetContactFoceMagnitude/APPhysNoTether/APPhysNoTether6PN_CFNM.m
@@ -0,0 +1,38 @@
+%% Facet Contact Force Magnitude
+% 6 P/N APPhys No tether 
+% file = 6NP_APPhysNoTether_facetForceMagnitude.xls
+figure (3)
+
+Data = xlsread('6NP_APPhysNoTether_facetForceMagnitude');  % importing the CFNM columns for each facet pair
+moment=Data(:,2);
+C6LR=Data(:,3);
+C6LL=Data(:,6);
+C5LR=Data(:,9);
+C5LL=Data(:,12);
+C4LR=Data(:,15);
+C4LL=Data(:,18);
+C3LR=Data(:,21);
+C3LL=Data(:,24);
+
+plot(moment,C6LR,'b')  % plotting Contact mean stress of each facet pair 
+hold on 
+plot(moment,C6LL,'m')
+hold on 
+plot(moment,C5LR,'g')
+legend('C6LL')
+hold on
+plot(moment,C5LL,'r')
+hold on 
+plot(moment,C4LR,'c')
+hold on 
+plot(moment,C4LL,'color',[.5,0,.5])
+hold on 
+plot(moment,C3LR,'k')
+hold on 
+plot(moment,C3LL)
+
+xlim([-2 2])
+xlabel('Moment (Nm)')
+ylabel('Newtons')
+title({'Facet Force Magnitude(CFNM) for APPhys NoTether model','under Axial rotation(6P/6N)'})
+legend('C6LR','C6LL','C5LR','C5LL','C4LR','C4LL','C3LR','C3LL','location','north')
---
+++ FacetContactFoceMagnitude/PhysPhysNoTether/PhysPhysNoTether4PN_CFNM.m
@@ -0,0 +1,38 @@
+%% Facet Contact Force Magnitude
+% 4 P/N PhysPhys No Tether 
+% file = 4NP_PhysPhysNoTether_facetForceMagnitude.xls
+figure (6)
+
+Data = xlsread('4NP_PhysPhysNoTether_facetForceMagnitude');  % importing the CFNM columns for each facet pair
+moment=Data(:,2);
+C6LR=Data(:,3);
+C6LL=Data(:,6);
+C5LR=Data(:,9);
+C5LL=Data(:,12);
+C4LR=Data(:,15);
+C4LL=Data(:,18);
+C3LR=Data(:,21);
+C3LL=Data(:,24);
+
+plot(moment,C6LR,'b')  % plotting Contact mean stress of each facet pair 
+hold on 
+plot(moment,C6LL,'m')
+hold on 
+plot(moment,C5LR,'g')
+legend('C6LL')
+hold on
+plot(moment,C5LL,'r')
+hold on 
+plot(moment,C4LR,'c')
+hold on 
+plot(moment,C4LL,'color',[.5,0,.5])
+hold on 
+plot(moment,C3LR,'k')
+hold on 
+plot(moment,C3LL)
+
+xlim([-2 2])
+xlabel('Moment (Nm)')
+ylabel('Newtons')
+title({'Facet Force Magnitude(CFNM) for PhysPhys NoTether model','under Flexion/Extention(4P/4N)'})
+legend('C6LR','C6LL','C5LR','C5LL','C4LR','C4LL','C3LR','C3LL','location','north')
